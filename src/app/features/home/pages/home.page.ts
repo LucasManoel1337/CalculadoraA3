d69afb438c9351395b278ad687bbfee56702b376
lmpaixao <lmpaixao@minsait.com>
2025-11-28 21:43:13 -0300

    Criação da tela Home e IMC

--- src/app/features/home/pages/home.page.ts
+++ src/app/features/home/pages/home.page.ts
@@ -1,10 +1,11 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgFor],
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.css'],
 })
@@ -14,4 +15,17 @@ export class HomePage {
   navegar(rota: string) {
     this.router.navigate([rota]);
   }
+
+  integrantes = [
+    'Lucas Manoel Paixão - RA: 12524140750',
+    'Victor Gabriel Monteiro Silva - RA: 1252411480',
+    'Igor Ferreira Zanotti - RA: 12524130207'
+  ];
+
+  descricao = `
+    Este projeto foi desenvolvido como parte da Avaliação A3 da disciplina Estruturas Matemáticas 
+    do professor Adjaci Uchoa Fernandes.
+    A proposta é criar uma calculadora/site completa contendo várias funcionalidades,
+    desde cálculos básicos até operações matemáticas mais elaboradas.
+  `;
 }
--- src/app/features/imc/pages/imc.page.ts
+++ src/app/features/imc/pages/imc.page.ts
@@ -1,10 +1,38 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-imc',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './imc.page.html',
-  styleUrls: ['./imc.page.css'],
+  styleUrl: './imc.page.css'
 })
-export class ImcPage {}
+export class ImcPage {
+  peso: number = 0;
+  altura: number = 0;
+  imc: number | null = null;
+  classificacao: string = "";
+  rotacaoPonteiro: string = "rotate(-90deg)"; // APARECENDO NASCIDO
+
+  calcularIMC() {
+    if (!this.peso || !this.altura) return;
+
+    this.imc = this.peso / (this.altura * this.altura);
+
+    if (this.imc < 18.5) this.classificacao = "Abaixo do peso";
+    else if (this.imc < 25) this.classificacao = "Peso normal";
+    else if (this.imc < 30) this.classificacao = "Acima do peso";
+    else this.classificacao = "Obeso";
+
+    this.rotacaoPonteiro = `rotate(${this.converterIMCparaAngulo(this.imc)}deg)`;
+  }
+
+  converterIMCparaAngulo(imc: number): number {
+    if (imc < 18.5) return -90 + (imc / 18.5) * 30;
+    if (imc < 25) return -60 + ((imc - 18.5) / (25 - 18.5)) * 60;
+    if (imc < 30) return 0 + ((imc - 25) / 5) * 45;
+    return 45 + Math.min((imc - 30) * 3, 45);
+  }
+}
